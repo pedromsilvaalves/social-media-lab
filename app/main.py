a3ed30d2e89859077b40a57f25d0b79e562ef4b5
Pedro Alves <pedromsilvaalves@gmail.com>
2022-08-07 18:24:27 -0300

    feat(post_api): setting up the api to use postgresql

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from typing import Optional
-from fastapi import Body, FastAPI, Response, status, HTTPException
+import time
+import psycopg2
+
+from psycopg2.extras import RealDictCursor
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
-from random import randrange
+
 
 app = FastAPI()
 
@@ -12,75 +15,110 @@ class post_model(BaseModel):
     published: bool = True
 
 
-my_posts = [
-    {"id": 1, "title": "Title 1", "content": "Content 1"},
-    {"id": 2, "title": "Title 2", "content": "Content 2"}
-]
+is_database_connected = False
 
-
-def find_post(id):
-    for post in my_posts:
-        if post["id"] == id:
-            return post
-    return None
-
-
-def find_post_index(id):
-    for index, post in enumerate(my_posts):
-        if post['id'] == id:
-            return index
-
-
-@app.get("/")
-def root():
-    return {"message": "Welcome to my API"}
+while not is_database_connected:
+    try:
+        conn = psycopg2.connect(
+            host="<hostname>",
+            dbname="<dbname>",
+            user="<db_username>",
+            password="<db_password>",
+            port=<db_port>,
+            cursor_factory=RealDictCursor
+        )
+        cursor = conn.cursor()
+        is_database_connected = True
+        print("Database connection was succesfull")
+    except Exception as error:
+        print("Database connection failed. Error: ", error)
+        time.sleep(2)
 
 
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts}
+    sql_query = """
+    SELECT * FROM posts    
+    """
+
+    cursor.execute(sql_query)
+    posts = cursor.fetchall()
+
+    return {"data": posts}
 
 
 @app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id)
+    sql_query = """
+    SELECT * FROM posts 
+        WHERE id = %s
+    """
+
+    cursor.execute(sql_query, (str(id),))
+    post = cursor.fetchone()
+
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} was not found"
         )
+
     return {"data": post}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: post_model):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 10000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    sql_query = """
+    INSERT INTO posts (title, content, published) VALUES (%s, %s, %s)
+    RETURNING *
+    """
+
+    cursor.execute(sql_query, (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+
+    return {"data": new_post}
 
 
 @app.put("/posts/{id}", status_code=status.HTTP_200_OK)
-def update_post(id: int, updated_post: post_model, response: Response):
-    post_index = find_post_index(id)
-    new_post_dict = updated_post.dict()
-    new_post_dict['id'] = id
+def update_post(id: int, post: post_model):
+    sql_query = """
+    UPDATE posts
+        SET
+            title = %s,
+            content = %s,
+            published = %s
+        WHERE 
+            id = %s
+    RETURNING *
+    """
 
-    if post_index:
-        my_posts[post_index] = new_post_dict
-        response.status_code = status.HTTP_200_OK
-    else:
-        response.status_code = status.HTTP_201_CREATED
-        my_posts.append(new_post_dict)
+    cursor.execute(sql_query, (post.title, post.content,
+                   post.published, str(id),))
+    updated_post = cursor.fetchone()
+    conn.commit()
 
-    return new_post_dict
+    if not updated_post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exists")
+
+    return updated_post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    post = find_post(id)
-    if not post:
+    sql_query = """
+    DELETE FROM posts
+        WHERE id = %s
+    RETURNING *
+    """
+
+    cursor.execute(sql_query, (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if not deleted_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exists")
-    my_posts.remove(post)
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
